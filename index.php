96f48867cf0f16ff235506fe79d558f21061e663
MelvinZacariasjn <53020805+MelvinZacariasjn@users.noreply.github.com>
2021-09-23 00:02:33 -0600

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
   </head>
-  <body><div class="p-3 mb-2 bg-secondary text-white"><h1>Formulario Empleados</h></div>
+  <body><div class="p-3 mb-2 bg-secondary text-white"><h1>Formulario Productos</h></div>
   
 
    
