d03653889c8eca740663a6948757208901b320ee
MelvinZacariasjn <53020805+MelvinZacariasjn@users.noreply.github.com>
2021-09-23 00:02:00 -0600

    Update index.php

--- index.php
+++ index.php
@@ -10,8 +10,10 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
   </head>
-  <body>
-    <h1>FORMULARIO PRODUCTOS</h1>
+  <body><div class="p-3 mb-2 bg-secondary text-white"><h1>Formulario Empleados</h></div>
+  
+
+   
     <div class="container">
     <form class="d-flex" action="crud_productos.php" method="post" >
       <div class="col">
